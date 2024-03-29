0e8a3136e0eab74bccc47862b9b6ef6d0ef7e710
Darlan0307 <darlanchagas2020@gmail.com>
2024-03-29 11:19:07 -0300

    :lipstick: ui da mensagem de agradecimento da compra finalizada

--- frontend/src/components/pages/MessageThanks/index.jsx
+++ frontend/src/components/pages/MessageThanks/index.jsx
@@ -1,15 +1,18 @@
+import { Link } from 'react-router-dom'
 import AnimationLottie from '../../AnimationLottie'
 import './style.css'
 
 const MessageThanks = () => {
   return (
-    <div>
-      <div>
+    <main className='container-thanks'>
+      <div className='container-animatejson'>
         <AnimationLottie/>
       </div>
       <h2>Compra Finalizada!</h2>
-      <p>Vamos verificar suas informações para realizarmos a entrega o mais rápido possível.</p>
-    </div>
+      <p>Ficamos felizes por escolher os nossos produtos.</p>
+      <p>Agora vamos verificar suas informações para realizarmos a entrega o mais rápido possível.</p>
+      <Link to="/">Voltar ao início</Link>
+    </main>
   )
 }
 
